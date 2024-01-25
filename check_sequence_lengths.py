e6fbb00de0b13614832808a84c9612d65017c349
AbrahamSanders <abraham.sanders@gmail.com>
2024-01-25 17:16:00 -0500

    fix check_sequence_lengths tool

--- check_sequence_lengths.py
+++ check_sequence_lengths.py
@@ -1,14 +1,17 @@
 from transformers import AutoTokenizer
 from tqdm import tqdm
 
-tokenizer = AutoTokenizer.from_pretrained("adept/persimmon-8b-base")
+from realtime_codec_agent.utils.tokenizer_utils import add_special_audio_tokens
+
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+add_special_audio_tokens(tokenizer, 2, 1024)
 
 # open data/audio/dataset_train.txt, tokenize each line, and compute the sequence length of each line.
 lengths = []
-with open("data/audio/dataset_dev_debug.txt", "r") as f:
+with open("data/audio/dataset_train.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
     for line in tqdm(lines):
-        tokens = tokenizer(line).input_ids
+        tokens = tokenizer(line.rstrip("\n")).input_ids
         lengths.append(len(tokens))
 
 # assert that they are all the same
