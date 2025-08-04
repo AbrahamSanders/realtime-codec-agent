e17bfbfb306f156051eff0d38a263c137e33a2e9
AbrahamSanders <abraham.sanders@gmail.com>
2025-08-04 04:22:33 -0400

    run_stream_codes tool supports mono/stereo and can paste in audio codes

--- run_stream_codes.py
+++ run_stream_codes.py
@@ -1,26 +1,32 @@
 import argparse
 import gradio as gr
 import numpy as np
+import itertools
 
 from realtime_codec_agent.audio_tokenizer import AudioTokenizer
 from realtime_codec_agent.utils.audio_utils import smooth_join, create_crossfade_ramps
-from codec_bpe import codes_to_chars, chars_to_codes, UNICODE_OFFSET_LARGE
+from codec_bpe import codes_to_chars, UNICODE_OFFSET_LARGE
 
 audio_tokenizer = None
 crossfade_ramps = None
 
-def stream_codes(audio, codes_file, encoding_chunk_size_secs, decoding_chunk_size_secs, length_secs):
+def stream_codes(audio, codes_file, audio_str, audio_str_is_stereo, encoding_chunk_size_secs, decoding_chunk_size_secs, length_secs):
     audio_tokenizer.reset_context()
 
-    if not audio and not codes_file:
-        raise ValueError("Either audio or codes_file must be provided.")
-    if audio and codes_file:
-        raise ValueError("Only one of audio or codes_file should be provided.")
+    if not audio and not codes_file and not audio_str:
+        raise ValueError("Either audio or codes_file or audio_str must be provided.")
+    if (audio and codes_file) or (audio and audio_str) or (codes_file and audio_str):
+        raise ValueError("Only one of audio, codes_file or audio_str should be provided.")
+    
+    mono_input = False
     if audio:
         # stream-encode the audio to codes
         sr, audio = audio
         if audio.ndim == 2:
             audio = audio.T
+        else:
+            audio = np.stack([audio, audio])
+            mono_input = True
         chunk_size_samples = int(encoding_chunk_size_secs * sr)
         encode_samples = min(int(length_secs * sr), audio.shape[-1]) if length_secs > 0 else audio.shape[-1]
         audio_str = ""
@@ -29,28 +35,36 @@ def stream_codes(audio, codes_file, encoding_chunk_size_secs, decoding_chunk_siz
             chunk = audio[..., start:end]
             chunk_str = audio_tokenizer.tokenize_audio((sr, chunk))
             audio_str += chunk_str
-        codes = chars_to_codes(
-            audio_str, 
-            audio_tokenizer.num_codebooks, 
-            audio_tokenizer.codebook_size, 
-            return_tensors="np", 
-            unicode_offset=UNICODE_OFFSET_LARGE,
-        )
-        codes = np.expand_dims(codes, axis=0)
     elif codes_file:
         codes = np.load(codes_file)
+        if codes.shape[0] == 1:
+            codes = np.concatenate([codes, codes], axis=0)
+            mono_input = True
+        channels_chars = [
+            codes_to_chars(
+                ch_codes, 
+                audio_tokenizer.codebook_size, 
+                unicode_offset=UNICODE_OFFSET_LARGE,
+            ) for ch_codes in codes
+        ]
+        audio_str = "".join(list(itertools.chain.from_iterable(zip(*channels_chars))))
+    elif audio_str and not audio_str_is_stereo:
+        channels_chars = [audio_str, audio_str]
+        audio_str = "".join(list(itertools.chain.from_iterable(zip(*channels_chars))))
+        mono_input = True
 
     # stream-decode the reconstruction
-    chunk_size_frames = int(decoding_chunk_size_secs * audio_tokenizer.framerate)
-    decode_frames = min(int(length_secs * audio_tokenizer.framerate), codes.shape[-1]) if length_secs > 0 else codes.shape[-1]
+    chunk_size_frames = int(decoding_chunk_size_secs * audio_tokenizer.framerate * audio_tokenizer.num_channels)
+    decode_frames = min(int(length_secs * audio_tokenizer.framerate * audio_tokenizer.num_channels), len(audio_str)) if length_secs > 0 else len(audio_str)
     audio = np.zeros((audio_tokenizer.num_channels, 0), dtype=np.float32)
     for start in range(0, decode_frames, chunk_size_frames):
         end = start + chunk_size_frames
-        chunk = codes[..., start:end]
-        audio_str = codes_to_chars(chunk[0], audio_tokenizer.codebook_size, unicode_offset=UNICODE_OFFSET_LARGE)
-        (_, output_audio), _, _ = audio_tokenizer.detokenize_audio(audio_str, preroll_samples=crossfade_ramps[0])
+        chunk = audio_str[start:end]
+        (_, output_audio), _, _ = audio_tokenizer.detokenize_audio(chunk, preroll_samples=crossfade_ramps[0])
         audio = smooth_join(audio, output_audio.reshape(audio_tokenizer.num_channels, -1), *crossfade_ramps)
-    return audio_tokenizer.sampling_rate, audio[0]
+    
+    audio = audio[0] if mono_input else audio.T
+    return audio_tokenizer.sampling_rate, audio
 
 
 if __name__ == "__main__":
@@ -62,7 +76,7 @@ if __name__ == "__main__":
 
     print(f"Running with args: {args}")
 
-    audio_tokenizer = AudioTokenizer(codec_model=args.codec_model, context_secs=args.context_secs)
+    audio_tokenizer = AudioTokenizer(codec_model=args.codec_model, num_channels=2, context_secs=args.context_secs)
     crossfade_ramps = create_crossfade_ramps(audio_tokenizer.sampling_rate, fade_secs=0.02)
 
     interface = gr.Interface(
@@ -70,6 +84,8 @@ if __name__ == "__main__":
         inputs=[
             gr.Audio(label="Audio"),
             gr.Textbox(label="Codes file"),
+            gr.Textbox(label="Codes string"),
+            gr.Checkbox(label="Codes string is stereo", value=False),
             gr.Slider(label="Encoding chunk size (seconds)", minimum=0.02, maximum=1.0, value=0.1, step=0.02),
             gr.Slider(label="Decoding chunk size (seconds)", minimum=0.02, maximum=1.0, value=0.1, step=0.02),
             gr.Slider(label="Length (seconds): zero for full length", minimum=0, maximum=120, value=30, step=1),
